55d62637299e5e91be7582b3fab7b236e957802d
Ethan Eddy <31893850+eeddy@users.noreply.github.com>
2021-03-27 12:23:35 -0300

    Update updateUser.sql

--- schema/updateUser.sql
+++ schema/updateUser.sql
@@ -1,8 +1,8 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS updateUser //
 
-CREATE PROCEDURE updateUser(IN userIdIn INT, IN firstNameIn VARCHAR(200), IN lastNameIn VARCHAR(200), IN profileUrlIn VARCHAR(500))
+CREATE PROCEDURE updateUser(IN userIdIn INT, IN firstNameIn VARCHAR(200), IN lastNameIn VARCHAR(200), IN profileUrlIn VARCHAR(500), IN isActiveIn BOOLEAN)
 BEGIN
-    UPDATE User SET firstName=firstNameIn, lastName=lastNameIn, profileImageUrl=profileUrlIn WHERE userId=userIdIn;
+    UPDATE User SET firstName=firstNameIn, lastName=lastNameIn, profileImageUrl=profileUrlIn, isActive=isActiveIn WHERE userId=userIdIn;
 END //
 DELIMITER ;
