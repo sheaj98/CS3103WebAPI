3b95f3e156f65704b7e3c84231e042b1a598576e
eeddy <eeddy@unb.ca>
2021-04-11 14:56:56 -0300

    Updates

--- schema/createLeague.sql
+++ schema/createLeague.sql
@@ -1,9 +1,9 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS createLeague //
 
-CREATE PROCEDURE createLeague(IN leagueNameIn VARCHAR(200), IN leagueFormatIdIn INT)
+CREATE PROCEDURE createLeague(IN leagueNameIn VARCHAR(200), IN descriptionIn VARCHAR(2000), IN leagueFormatIdIn INT)
 BEGIN
-    INSERT INTO League (name, LeagueFormat_leagueFormatId) VALUE (leagueNameIn, leagueFormatIdIn);
+    INSERT INTO League (name, description, LeagueFormat_leagueFormatId) VALUE (leagueNameIn, descriptionIn, leagueFormatIdIn);
     SELECT LAST_INSERT_ID();
 END //
 DELIMITER ;
